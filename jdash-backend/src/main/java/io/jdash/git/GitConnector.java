b4a3e5bb05a7ddd8e07a46b612a80cba7a5f2888
michal <msadowski.ib@gmail.com>
2018-07-17 21:46:54 +0200

    backend init commit

---
+++ jdash-backend/src/main/java/io/jdash/git/GitConnector.java
@@ -0,0 +1,7 @@
+package io.jdash.git;
+
+/**
+ * @author Michal Sadowski (sadochasee@gmail.com) on 16/07/2018.
+ */
+public class GitConnector {
+}
---
+++ jdash-backend/src/main/java/io/jdash/repository/Indexer.java
@@ -0,0 +1,9 @@
+package io.jdash.repository;
+
+/**
+ * @author Michal Sadowski (sadochasee@gmail.com) on 16/07/2018.
+ *
+ * @since 1.0
+ */
+public interface Indexer {
+}
